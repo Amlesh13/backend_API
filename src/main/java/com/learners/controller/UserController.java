deaf60d4e2353e266325449edbc03ef18a1efb2a
amlesh13 <amlesh13@gmail.com>
2017-02-01 15:43:39 +0530

    jvmidel branch added

--- src/main/java/com/learners/controller/UserController.java
+++ src/main/java/com/learners/controller/UserController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.learners.java.entities.ExternalFacingUser;
 import com.learners.service.interfaces.IUserService;
 import com.wordnik.swagger.annotations.Api;
